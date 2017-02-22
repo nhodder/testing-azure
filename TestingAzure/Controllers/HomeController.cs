f47a48fec7aaec66f10c45a245277a23bb31e2ce
nhodder <nhodder@live.com.au>
2017-02-22 22:35:05 +1030

    connect to database

--- TestingAzure/Controllers/HomeController.cs
+++ TestingAzure/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +14,30 @@ namespace TestingAzure.Controllers
         // GET: /Home/
         public ActionResult Index()
         {
+            List<Tuple<string, string>> records = new List<Tuple<string, string>>();
+            string connString = ConfigurationManager.ConnectionStrings["TestAzure"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(connString))
+            {
+                if (connection.State != System.Data.ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+
+                string query = "select Name, Url from dbo.Blogs";;
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            records.Add(new Tuple<string, string>(reader.GetString(0), reader.GetString(1)));
+                        }
+                    }
+                }
+            }
+
+            ViewBag.Records = records;
+
             return View();
         }
 	}
